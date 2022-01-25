82d52ba3713ff19d1a2fed3930cf6ceb89acb41c
bdfries <basedevelfries@email.cz>
2022-01-25 18:02:24 +0100

    Creating the Server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Initial Commit
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3002',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', socket => {
+
+});
+
+server.listen(3004, () => {
+    console.log('Listening on port 3004');
+});
